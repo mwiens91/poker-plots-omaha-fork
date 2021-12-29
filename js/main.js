f6e90dc0900140d8321e1084ac9d0b82558915d6
Matt Wiens <mwiens91@gmail.com>
2021-12-29 15:29:24 -0500

    Fixup URL in main JS function
    Should no longer be pointing locally

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ import { drawBoxPlot } from "./modules/boxplot.mjs";
 import { drawLinePlot } from "./modules/lineplot.mjs";
 
 // Draw the graph after fetching data
-fetch("../data/data.min.json")
+// fetch("../data/data.min.json") // uncomment this to use local version
+fetch("https://mwiens91.github.io/poker-plots/data/data.min.json")
   .then((response) => response.json())
   .then((data) => {
     drawLinePlot(data, "line-plot", 950, 60, 40);
