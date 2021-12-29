5e354f01107818408b1abb861d0de73202c8220b
Matt Wiens <mwiens91@gmail.com>
2021-12-29 15:34:36 -0500

    Add some convenience scripts

---
+++ scripts/1_make_data_and_html.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./make_data.py
+./make_html.js
---
+++ scripts/2_do_1_but_also_commit_and_push.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+./1_make_data_and_html.sh
+
+git add ../index.html
+git add data
+
+git commit -m "Add new data"
