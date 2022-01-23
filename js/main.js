351e4a939a16beebef305955d4904411cb58bdf8
Matt Wiens <mwiens91@gmail.com>
2022-01-22 19:11:45 -0500

    Clean up an errant console.log

--- js/main.js
+++ js/main.js
@@ -54,7 +54,6 @@ fetch("https://mwiens91.github.io/poker-plots/data/data.min.json")
 
 // Don't show calendar block if window is small
 window.addEventListener("resize", () => {
-  console.log(document.documentElement.clientWidth)
   if (document.documentElement.clientWidth < 950) {
     document.getElementById("calendar").style.display = "none"
   } else {
