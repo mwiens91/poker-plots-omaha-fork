551a91cec0609b1bcdb05586fd6ff85e4cb0d99a
Matt Wiens <mwiens91@gmail.com>
2022-04-21 02:53:37 -0400

    Check for local changes before pushing in script

--- scripts/2_do_1_but_also_commit_and_push.sh
+++ scripts/2_do_1_but_also_commit_and_push.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-# The prompt is thanks to Dennis here:
+if [[ `git status --porcelain --untracked-files=no` ]]; then
+  echo "Aborting. There are uncommited changes in this repository."
+  exit 1
+else
+  :
+fi
+
+# The prompt code is thanks to Dennis here:
 # https://stackoverflow.com/a/3232082
 read -r -p "You are about to push a commit upstream. Is this what you want? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
-  git stash
   git pull
-  git stash pop
 
   ./1_make_data_and_html_and_min_js.sh
 
