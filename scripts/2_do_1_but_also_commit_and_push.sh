f203f5309a8e9c72af98128cfbac614b8a8a3eae
Matt Wiens <mwiens91@gmail.com>
2022-04-21 02:55:49 -0400

    Change prompt order in pushing script

--- scripts/2_do_1_but_also_commit_and_push.sh
+++ scripts/2_do_1_but_also_commit_and_push.sh
@@ -7,23 +7,23 @@ else
   :
 fi
 
+git pull
+
+./1_make_data_and_html_and_min_js.sh
+
+git add ../index.html
+git add ../data/raw
+git add ../data/data.json
+git add ../data/data.min.json
+git add ../js/modules/dataModule.mjs
+git add ../js/pokerplots.min.js
+
 # The prompt code is thanks to Dennis here:
 # https://stackoverflow.com/a/3232082
 read -r -p "You are about to push a commit upstream. Is this what you want? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
-  git pull
-
-  ./1_make_data_and_html_and_min_js.sh
-
-  git add ../index.html
-  git add ../data/raw
-  git add ../data/data.json
-  git add ../data/data.min.json
-  git add ../js/modules/dataModule.mjs
-  git add ../js/pokerplots.min.js
-
   git commit -m "Add new data"
   git push
 else
